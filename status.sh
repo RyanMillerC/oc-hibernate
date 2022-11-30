7cd2980758d4859b383e8a8a5bf2c68076ba9d23
Ryan Miller <miller@redhat.com>
2022-11-30 15:47:09 -0500

    Remove start/stop scripts, replace with status script

---
+++ status.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+INSTANCES=$(aws ec2 describe-instances | jq -r '.Reservations[].Instances')
+
+INSTANCE_NAMES=$(echo "$INSTANCES" | jq -r '.[].Tags[].Value' | sed '/^cluster-*/!d')
+INSTANCE_STATUSES=$(echo "$INSTANCES" | jq -r '.[].State.Name')
+
+# echo $INSTANCES | jq -r '.[].Tags[].Value as $value | if ($value == "cluster-.*") then $value else $value end' # , .[].State.Name' # | sed '/^cluster-*/!d'
+
+echo $INSTANCES | jq -r '.[] | .State.Name, (.Tags[] | select(.Value | test("^cluster-.*")) | .Value)'
+
+# OLD_IFS="$IFS"
+# IFS="\n"
+# for INDEX in "${!INSTANCE_NAMES[@]}" ; do
+#     echo $INDEX
+#     echo "${INSTANCE_NAMES[INDEX]} ${INSTANCE_STATUSES[INDEX]}"
+# done
+# IFS="$OLD_IFS"
