3159430551d5af3027b381aadcfa7f709e1b9e28
Ryan Miller <miller@redhat.com>
2022-12-01 09:38:54 -0500

    Use click instead of passing args directly

--- cli.py
+++ cli.py
@@ -3,4 +3,4 @@ import sys
 from hibernate.__main__ import main
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
--- hibernate/__main__.py
+++ hibernate/__main__.py
@@ -1,5 +1,9 @@
-def main(args):
-    if len(args) > 1:
-        print(f"{args[0]}: Hello {args[1]}!")
+import click
+
+@click.command()
+@click.option("--name", help="Will display in message")
+def main(name):
+    if name:
+        print(f"Hello {name}!")
     else:
-        print(f"{args[0]}: Hello World!")
+        print(f"Hello World!")
