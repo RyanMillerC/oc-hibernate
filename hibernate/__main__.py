ecefc60984c6ff8ce06e854623a24604b573ee05
Ryan Miller <miller@redhat.com>
2022-12-01 21:13:44 -0500

    Call AWS cli to list instance statuses

--- hibernate/__main__.py
+++ hibernate/__main__.py
@@ -1,3 +1,4 @@
+import json
 import sys
 
 import click
@@ -23,7 +24,28 @@ cli.add_command(print_message)
 
 @click.command(help="Print hibernation status")
 def status():
-    print(f"STATUS GOES HERE")
+    aws_cmd_output = sh.aws('ec2', 'describe-instances', '--output', 'json', _tty_out=False)
+    aws_response = json.loads(aws_cmd_output.stdout)
+    ec2_instances = [
+        reservation["Instances"][0] for reservation in aws_response["Reservations"]
+    ]
+
+    filtered_instances = []
+    for ec2_instance in ec2_instances:
+        name = "Unknown"
+        for tag in ec2_instance["Tags"]:
+            if tag['Key'] == "Name":
+                name = tag['Value']
+        state = ec2_instance['State']['Name']
+        filtered_instances.append({
+            "Name": name,
+            "State": state,
+        })
+
+    template = "{Name:50}{State:10}"
+    print(template.format(Name="NAME", State="STATE"))
+    for instance in filtered_instances:
+        print(template.format(**instance))
 cli.add_command(status)
 
 
