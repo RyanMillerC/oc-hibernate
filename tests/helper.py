175b8f3af26252b930d7553a5ddc0df68c58c87c
Ryan Miller <miller@redhat.com>
2022-12-30 21:05:52 -0500

    Support comments in JSON test files

--- tests/helper.py
+++ tests/helper.py
@@ -6,5 +6,9 @@ import json
 def load_json_file(file_path):
     """Return a dictionary object loaded from a JSON file."""
     with open(file_path, "r") as stream:
-        json_data = json.load(stream)
+        # Remove lines from JSON file starting with //
+        json_removed_comments = ''.join(
+            line for line in stream if not line.startswith('//')
+        )
+        json_data = json.loads(json_removed_comments)
     return json_data
