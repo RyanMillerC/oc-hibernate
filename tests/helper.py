61bcb771c0e9846a8088783ab9c6bb7791b46c49
Ryan Miller <miller@redhat.com>
2022-12-30 21:10:41 -0500

    Support JSON comments with leading whitespace

--- tests/helper.py
+++ tests/helper.py
@@ -4,11 +4,15 @@ import json
 
 
 def load_json_file(file_path):
-    """Return a dictionary object loaded from a JSON file."""
+    """Return a dictionary object loaded from a JSON file.
+
+    Supports JSON files with comments as long as the line starts with "//" with
+    any amount of leading spaces.
+    """
     with open(file_path, "r") as stream:
         # Remove lines from JSON file starting with //
         json_removed_comments = ''.join(
-            line for line in stream if not line.startswith('//')
+            line for line in stream if not line.lstrip().startswith('//')
         )
         json_data = json.loads(json_removed_comments)
     return json_data
